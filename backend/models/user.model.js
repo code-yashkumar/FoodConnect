a1938cc9c55a5ff4abace7a48911cbac321ef464
code-yashkumar <code.yashkumar@gmail.com>
2026-08-22 03:50:50 +0530

    Initial Commit

---
+++ backend/models/user.model.js
@@ -0,0 +1,28 @@
+const mongoose = require("mongoose");
+const userSchemma = new mongoose.Schema({
+    fullname:{
+        type:String,
+        required:true
+    },
+    email:{
+        type:String,
+        required:true,
+        unique:true
+    },
+    password:{
+        type:String
+    },
+    mobile:{
+        type:String,
+        required:true
+    },
+    role:{
+        type:String,
+        enum:["user","owner","deliveryBoy"],
+        required:true
+    }
+},{ timestamps:true});
+
+
+const User = mongoose.model("User",userSchemma);
+export default User; 
